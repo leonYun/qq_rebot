9d4d28ffb679cca97bcdc0e1c1bacd1373c12c0b
leonYun <1037047204@qq.com>
2022-05-29 09:30:43 +0800

    fix

--- service/src/main/java/com/leon/project/qqRobot/service/service/model/QQWarningRecord.java
+++ service/src/main/java/com/leon/project/qqRobot/service/service/model/QQWarningRecord.java
@@ -18,7 +18,7 @@ import javax.persistence.*;
 )
 public class QQWarningRecord {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String warningId;
